58383b1c08a231dff1788f3e5155263b605257d8
Santiago Valencia <santivv2414@gmail.com>
2025-02-24 20:37:51 -0500

    esto es una prueba

--- Curso python desde 0/Hola mundo/htmlmaping/ma.py
+++ Curso python desde 0/Hola mundo/htmlmaping/ma.py
@@ -1,10 +1,12 @@
 import folium
 import webbrowser
+import pandas
+data = pandas.read_csv
 mapa = folium.Map(location=[80, -100], zoom_start=6, tiles="OpenStreetMap")
 
 fg = folium.FeatureGroup(name="My map")
 
-for cordinates in range(5)[[34.5,29.5],[35.4,66.9]] :
+for cordinates in [[34.5,29.5],[35.4,66.9]] :
     mapa.add_child(folium.Marker(location=cordinates, popup="Hola, soy un marcador", icon=folium.Icon(color='red')))
 
 mapa.save("Map1.html")
--- student_attendance/frontend_flet/api_client.py
+++ student_attendance/frontend_flet/api_client.py
@@ -1,12 +1,1 @@
-import httpx
-
-API_URL = "http://localhost:8000"
-
-def obtener_estudiantes():
-    try:
-        response = httpx.get(f"{API_URL}/students")
-        response.raise_for_status()  # Lanza error si la respuesta no es 200
-        return response.json()
-    except httpx.HTTPError as e:
-        print(f"Error al obtener estudiantes: {e}")
-        return []
+import flet
