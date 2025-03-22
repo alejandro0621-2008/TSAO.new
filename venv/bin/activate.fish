2ffa5eb1355b4d197b9256dc1a67627a967fabf0
Santiago Valencia <santivv2414@gmail.com>
2025-03-21 23:19:53 -0500

    elimine venv pq es inecesario

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/santiago/Descargas/recuperacion/recurperacion/TSAO/venv
+set -gx VIRTUAL_ENV /home/santiago/Descargas/recuperacion/recurperacion/TSAO.new/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
